123de18e61b3a2c4805434fe76a06b6391c8e2c2
Omkar Kaul <nzomkar@gmail.com>
2021-10-18 01:01:51 +1300

    Recreate certificate in us-east-1 to be compatible with Cloudfront

--- infra/service/cert.tf
+++ infra/service/cert.tf
@@ -1,4 +1,14 @@
+# https://docs.aws.amazon.com/acm/latest/userguide/acm-services.html
+# "To use an ACM Certificate with CloudFront, you must request or import the certificate in the US East (N. Virginia) region."
+# https://www.terraform.io/docs/configuration/providers.html#multiple-provider-instances
+
+provider "aws" {
+  alias      = "acm_provider"
+  region     = "us-east-1"
+}
+
 resource "aws_acm_certificate" "website_cert" {
+  provider          = aws.acm_provider
   domain_name       = "www.rakmo.io"
   validation_method = "DNS"
   subject_alternative_names = ["rakmo.io"]
