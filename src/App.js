81e3bf88bcc5f322928e32a166cd69ccdd01e913
Omkar Kaul <nzomkar@gmail.com>
2021-10-17 19:20:12 +1300

    Init WIP website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+
+function generateFaqBlob(question, answer) {
+  return (
+    <div className="faqBody">
+      <p className="question"><b>Q</b>: {question}</p>
+      <p className="answer"><b>A</b>: {answer}</p>
+    </div>
+    )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contentContainer">
+
+        <div className="headerContainer">
+          <h1 className="title">rakmo.io</h1>
+          <div className="linkContainer">
+            <a className="link" href="https://www.github.com/omkarkaul">Github</a>
+            <a className="link" href="https://www.linkedin.com/in/omkar-kaul/">LinkedIn</a>
+          </div>
+        </div>
+
+        <div className="subtitleContainer">
+          <h1 className="subtitle">
+            This site is under construction
+          </h1>
+        </div>
+
+        <div className="main">
+          <h3 className="mainTitle">FAQ</h3>
+          {generateFaqBlob("What's your name?", "Omkar Kaul")}
+          {generateFaqBlob("What did you study?", "I studied a Computer Science and Economics conjoint degree at the University of Auckland")}
+          {generateFaqBlob("What do you do?", "I work as a Software Engineer at Xero")}
+          {generateFaqBlob("What kind of things do you enjoy in Software Engineering?", "I really enjoy DevOps, software automation, SRE, and product enablement")}
+          {generateFaqBlob("When will this website be done?", "Ticket is in the backlog")}
+          {generateFaqBlob("What's the deal with the website name?", "The website name spelled backwards is what my friends usually say to get my attention")}
+        </div>
+      </div>
     </div>
   );
 }
