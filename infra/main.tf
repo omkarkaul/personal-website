08b806fa3000331d0c69ac6f3d4ad650e33ee1e1
Omkar Kaul <nzomkar@gmail.com>
2023-10-31 20:25:23 +1100

    Add PR preview URL to CORS origin rules

--- infra/main.tf
+++ infra/main.tf
@@ -42,7 +42,8 @@ resource "aws_s3_bucket_cors_configuration" "example" {
     allowed_methods = ["HEAD", "GET"]
     allowed_origins = [
       "https://www.rakmo.io",
-      "http://localhost:3000"
+      "http://localhost:3000",
+      "https://*rakmo.netlify.app"
     ]
   }
 }
