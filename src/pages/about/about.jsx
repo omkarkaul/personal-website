9056c4bae4de8bf3bf0f27edf99748ef26644bc6
Omkar Kaul <nzomkar@gmail.com>
2023-10-31 19:23:45 +1100

    Allow prettier word wrap for question & answer

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -3,20 +3,45 @@ import {retrieveObjectFromS3} from "../../utils";
 import {CONSTANTS} from "../../constants";
 
 function About() {
-    const [qna, setQna] = useState("");
+    const [qna, setQna] = useState([]);
 
     const fetchQna = () => {
         retrieveObjectFromS3(CONSTANTS.QNA_KEY, CONSTANTS.WEBSITE_BUCKET, CONSTANTS.REGION)
-            .then(object => setQna(object));
+            .then(object => setQna(JSON.parse(object)));
     }
 
     useEffect(() => {
         fetchQna();
     }, []);
 
+    const buildQnaThingToRender = () => {
+        return (
+            <>
+                <p>{"["}</p>
+                {qna.map((qnaItem, index) =>
+                    <div key={index} className="qna">
+                        <p>{"{"}</p>
+                        <div className="qnaQuestionContainer">
+                            <p className="qnaQuestion">"question": </p>
+                            <p> "{qnaItem.question}</p>
+                        </div>
+                        <div className="qnaAnswerContainer">
+                            <p className="qnaAnswer">"answer": </p>
+                            <p> "{qnaItem.answer}"</p>
+                        </div>
+                        <p>{index === qna.length - 1 ? "}" : "},"}</p>
+                    </div>
+                )}
+                <p>{"]"}</p>
+            </>
+        )
+    }
+
     return (
-        <div>
-            <pre>{qna}</pre>
+        <div className="qnaContainer">
+            {qna.length === 0 ?
+                <p className="loading">loading...</p> :
+                buildQnaThingToRender(qna)}
         </div>
     )
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div>
-            welcome to my world
+            <p>welcome to my world</p>
         </div>
     )
 }
