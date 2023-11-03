8b2920fb272fad0c5fc71ad5f193a4a79e4faf97
Omkar Kaul <nzomkar@gmail.com>
2023-11-03 15:09:42 +1100

    Fix page not loading when clicking browser back/forward buttons, Make route top level component rendered by index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Router from "./components/router";
 
 ReactDOM.render(
-    <App />,
+    <Router />,
   document.getElementById('root')
 );
